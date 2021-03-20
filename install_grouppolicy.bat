9ed13d64804ce98f9fa398ef49c15e4cd51c1861
aoirint <aoirint@gmail.com>
2021-03-20 17:54:01 +0900

    install group policy editor script

---
+++ install_grouppolicy.bat
@@ -0,0 +1,12 @@
+rem https://www.japan-secure.com/entry/how-to-use-local-group-policy-editor-in-windows-10-home.html
+
+@echo off 
+pushd "%~dp0" 
+
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum >List.txt 
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum >>List.txt 
+
+for /f %%i in ('findstr /i . List.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i" 
+pause
+
+
